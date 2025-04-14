55f67629756f3c03ad9f1ebd428b0756e8c5b3c7
Vince <jpinard@bluewin.ch>
2025-04-14 13:10:35 +0200

    bug fix

--- src/data/api.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "https://chabloz.eu/files/horaires/";
-
-const loadJson = (url) => fetch(url).then((response) => response.json());
-
-const loadSchedule = () => loadJson(`${BASE_URL}/all`);
-
-export { loadSchedule };
